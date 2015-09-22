14eb908ca1a0366431f94d25266c4b1ea5b1334a
Ram Ganesh <ramganes@amtexsystems.com>
2015-09-22 22:03:06 +0530

    Started Chapter-4

--- masteringdjango/views.py
+++ masteringdjango/views.py
@@ -3,6 +3,7 @@ import datetime
 from django.template.loader import get_template
 from django.template import Context
 from django.http import Http404, HttpResponse
+from django.shortcuts import render
 
 def hello(request):
 	return HttpResponse('Hello world')
@@ -10,10 +11,11 @@ def hello(request):
 
 def current_datetime(request):
 	now = datetime.datetime.now()
-	t = get_template('current_datetime.html')
-	html = t.render(Context({'current_date': now}))
-	return HttpResponse(html)
+	# t = get_template('current_datetime.html')
+	# html = t.render(Context({'current_date': now}))
 
+	# return HttpResponse(html)
+	return render(request, 'current_datetime.html', {'current_date': now})
 
 def hours_ahead(request, offset):
 	try:
@@ -21,5 +23,6 @@ def hours_ahead(request, offset):
 	except ValueError:
 		raise Http404()
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-	return HttpResponse(html)
+	# html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+	# return HttpResponse(html)
+	return render(request, 'hours_ahead.html', {'hour_offset': offset, 'next_time':dt})
