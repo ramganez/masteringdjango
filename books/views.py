d77ddceec9df1935d90b91b822f0b836110dbb96
Ram Ganesh <ramganes@amtexsystems.com>
2015-10-01 18:47:54 +0530

    Finished 1st half in Chapter 6

--- books/views.py
+++ books/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from books.models import Book
 # Create your views here.
+
+
+def search_form(request):
+    return render(request, 'search_form.html')
+
+
+# def search(request):
+#     if 'q' in request.GET:
+#         message = 'You searched for: %r' % request.GET['q']
+#     else:
+#         message = 'You submitted an empty form.'
+#     return HttpResponse(message)
+
+
+def search(request):
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        return render(request, 'search_results.html',
+            {'books': books, 'query': q})
+    else:
+        return render(request, 'search_form.html', {'error': True })
--- masteringdjango/urls.py
+++ masteringdjango/urls.py
@@ -17,10 +17,16 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from masteringdjango.views import (hello, current_datetime,
                                    hours_ahead)
+
+from books import views
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^search_form/$', views.search_form),
+    url(r'^search/$', views.search),
+
 ]
 
